aed16e0025c2a8392a624adca86b903845933386
ParrishJ <jarparrish@gmail.com>
2019-06-24 17:12:15 -0700

    fixed cors issue

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const server = express();
+
 
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
 
-const server = express();
+
 
 
 const usersRouter = require('../users/users-router');
