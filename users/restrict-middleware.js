72b09c1f44546420ccb651f03f4dae414e6a0c63
ParrishJ <jarparrish@gmail.com>
2019-06-25 16:12:12 -0700

    a

--- users/restrict-middleware.js
+++ users/restrict-middleware.js
@@ -10,7 +10,7 @@ function authenticate(req, res, next) {
     const token = req.headers.authorization
 
     if (token) {
-        jwt.verify(token, secret.jwtSecret, (err, decodeToken) => {
+        jwt.verify(token, secret.jwtSecret, (err, decodedToken) => {
             if (err) {
                 res.status(401).json({ message: 'Invalid Credentials' });
             } else {
